5860db20ec5bd7adf44439a51d88b463126f254f
StefanTusjak <stefan.tusjak@gmail.com>
2025-05-18 17:16:58 +0200

    Přidána funkce addition()

---
+++ 01_Calculator/calculator.py
@@ -0,0 +1,12 @@
+def addition(number1: float, number2: float) -> float:
+    """
+    Funkce sečte dvě desetinná čísla a vrátí jejich součet
+
+    Args:
+        number1 (float): První číselný vstup
+        number2 (float): Druhý číselný vstup
+
+    Returns:
+        float: Výsledek součtu number1 a number2 
+    """
+    return number1 + number2
